18520556d54b6edbd2cd4e8883ec668901f741a3
david <mrminecraft00@gmail.com>
2024-03-28 18:36:33 +0100

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,2 @@
-plugins {
-    id("java")
-}
-
 group = "net.thenextlvl.protect"
-version = "1.0.0"
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-tasks {
-    compileJava {
-        options.encoding = Charsets.UTF_8.name()
-        options.release.set(17)
-    }
-    javadoc {
-        options.encoding = Charsets.UTF_8.name()
-    }
-    processResources {
-        filteringCharset = Charsets.UTF_8.name()
-    }
-}
+version = "2.0.0"
