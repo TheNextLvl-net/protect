4ff84d48cb9317e9e60726024c50d62ca04a6d27
david <mrminecraft00@gmail.com>
2024-03-30 20:57:28 +0100

    implemented comparable

--- plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlag.java
+++ plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlag.java
@@ -3,5 +3,10 @@ package net.thenextlvl.protect.flag;
 import org.bukkit.NamespacedKey;
 import org.jetbrains.annotations.NotNull;
 
-public record CraftFlag<T>(@NotNull NamespacedKey key, T defaultValue) implements Flag<T> {
+public record CraftFlag<T>(@NotNull NamespacedKey key, @NotNull Class<? extends T> type, T defaultValue) implements Flag<T> {
+
+    @Override
+    public int compareTo(@NotNull Flag<?> flag) {
+        return key().compareTo(flag.key());
+    }
 }
