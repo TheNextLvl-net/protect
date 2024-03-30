b803a407c631c07d205074b14976c4214607431f
david <mrminecraft00@gmail.com>
2024-03-30 23:27:57 +0100

    added enum flag support

--- plugin/src/main/java/net/thenextlvl/protect/command/AreaFlagCommand.java
+++ plugin/src/main/java/net/thenextlvl/protect/command/AreaFlagCommand.java
@@ -16,10 +16,7 @@ import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
@@ -76,6 +73,8 @@ class AreaFlagCommand {
                                 return IntStream.rangeClosed(0, 100).mapToObj(Float::toString).toList();
                             else if (flag.type().equals(Long.class))
                                 return IntStream.rangeClosed(0, 100).mapToObj(Long::toString).toList();
+                            else if (flag.type().isEnum())
+                                return Arrays.stream(flag.type().getEnumConstants()).map(String::valueOf).toList();
                             return Collections.emptyList();
                         }).greedy().asOptional().build())
                 .handler(this::execute);
@@ -101,21 +100,22 @@ class AreaFlagCommand {
 
     private void handleSet(CommandContext<CommandSender> context, CommandSender sender, @Nullable Flag<Object> flag, @Nullable Area area) {
         var value = context.contains("value") ? context.<String>get("value") : null;
-        if (flag == null || area == null || value == null) {
+        if (flag == null || area == null || value == null) plugin.bundle().sendMessage(sender, "command.area.flag");
+        else try {
+            if (setFlagValue(flag, area, value)) plugin.bundle().sendMessage(sender, "area.flag.changed",
+                    Placeholder.parsed("value", value),
+                    Placeholder.parsed("area", area.getName()),
+                    Placeholder.parsed("flag", flag.key().asString()));
+            else plugin.bundle().sendMessage(sender, "nothing.changed");
+        } catch (Exception e) {
             plugin.bundle().sendMessage(sender, "command.area.flag");
-            return;
         }
-        if (setFlagValue(flag, area, value)) plugin.bundle().sendMessage(sender, "area.flag.changed",
-                Placeholder.parsed("value", value),
-                Placeholder.parsed("area", area.getName()),
-                Placeholder.parsed("flag", flag.key().asString()));
-        else plugin.bundle().sendMessage(sender, "nothing.changed");
     }
 
-    private boolean setFlagValue(@NotNull Flag<Object> flag, @NotNull Area area, String value) {
+    private boolean setFlagValue(@NotNull Flag<Object> flag, @NotNull Area area, @NotNull String value) {
         if (flag.type().equals(Boolean.class)) {
             var state = Boolean.parseBoolean(value);
-            if (!area.getFlag(flag).equals(state)) area.setFlag(flag, state);
+            if (!Objects.equals(area.getFlag(flag), state)) area.setFlag(flag, state);
             else return false;
         } else if (flag.type().equals(Integer.class)) {
             var state = Integer.parseInt(value);
@@ -131,11 +131,16 @@ class AreaFlagCommand {
             else return false;
         } else if (flag.type().equals(Long.class)) {
             var state = Long.parseLong(value);
-            if (Objects.equals(state, area.getFlag(flag))) area.setFlag(flag, state);
+            if (!Objects.equals(state, area.getFlag(flag))) area.setFlag(flag, state);
             else return false;
         } else if (flag.type().equals(String.class)) {
             if (!Objects.equals(area.getFlag(flag), value)) area.setFlag(flag, value);
             else return false;
+        } else if (flag.type().isEnum()) {
+            var state = Arrays.stream(flag.type().getEnumConstants())
+                    .filter(o -> String.valueOf(o).equalsIgnoreCase(value))
+                    .findAny().orElseThrow();
+            if (!Objects.equals(area.getFlag(flag), state)) area.setFlag(flag, state);
         } else return false;
         return true;
     }
