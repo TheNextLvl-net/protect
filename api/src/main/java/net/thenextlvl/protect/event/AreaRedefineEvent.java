6cc38a61ce08a5a526d72ce8c3012d0f828d6ff3
david <mrminecraft00@gmail.com>
2024-03-30 23:24:14 +0100

    use type parameters

--- api/src/main/java/net/thenextlvl/protect/event/AreaRedefineEvent.java
+++ api/src/main/java/net/thenextlvl/protect/event/AreaRedefineEvent.java
@@ -3,16 +3,16 @@ package net.thenextlvl.protect.event;
 import com.sk89q.worldedit.regions.Region;
 import lombok.Getter;
 import lombok.Setter;
-import net.thenextlvl.protect.area.Area;
+import net.thenextlvl.protect.area.RegionizedArea;
 import org.bukkit.event.Cancellable;
 
 @Getter
 @Setter
-public class AreaRedefineEvent extends AreaEvent implements Cancellable {
-    private final Region region;
+public class AreaRedefineEvent<T extends Region> extends AreaEvent implements Cancellable {
+    private final T region;
     private boolean cancelled;
 
-    public AreaRedefineEvent(Area area, Region region) {
+    public AreaRedefineEvent(RegionizedArea<T> area, T region) {
         super(area);
         this.region = region;
     }
