daef2ac4c563de47d0600bf823f19c0856fba790
david <mrminecraft00@gmail.com>
2024-03-30 20:50:55 +0100

    fixed leave not being triggered

--- plugin/src/main/java/net/thenextlvl/protect/listener/MovementListener.java
+++ plugin/src/main/java/net/thenextlvl/protect/listener/MovementListener.java
@@ -10,19 +10,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
+import java.util.stream.Stream;
+
 @RequiredArgsConstructor
 public class MovementListener implements Listener {
     private final ProtectPlugin plugin;
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (event.hasChangedBlock()) plugin.areaProvider().getAreas(event.getTo()).sorted().forEach(area -> {
-            if (!area.contains(event.getFrom()) && area.contains(event.getTo())) {
-                event.setCancelled(!new AreaEnterEvent(event.getPlayer(), area).callEvent());
-            } else if (area.contains(event.getFrom()) && !area.contains(event.getTo())) {
-                event.setCancelled(!new AreaLeaveEvent(event.getPlayer(), area).callEvent());
-            }
-        });
+        if (!event.hasChangedBlock()) return;
+        Stream.concat(plugin.areaProvider().getAreas(event.getFrom()), plugin.areaProvider().getAreas(event.getTo()))
+                .forEach(area -> {
+                    if (!area.contains(event.getFrom()) && area.contains(event.getTo())) {
+                        event.setCancelled(!new AreaEnterEvent(event.getPlayer(), area).callEvent());
+                    } else if (area.contains(event.getFrom()) && !area.contains(event.getTo())) {
+                        event.setCancelled(!new AreaLeaveEvent(event.getPlayer(), area).callEvent());
+                    }
+                });
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
