fd9e281a3a95982f27166a9f5ba5ec131fc40b52
david <mrminecraft00@gmail.com>
2024-04-06 09:16:32 +0200

    added owner to RegionizedArea

--- api/src/main/java/net/thenextlvl/protect/area/RegionizedArea.java
+++ api/src/main/java/net/thenextlvl/protect/area/RegionizedArea.java
@@ -5,14 +5,9 @@ import core.annotation.MethodsReturnNotNullByDefault;
 import core.annotation.ParametersAreNotNullByDefault;
 import core.annotation.TypesAreNotNullByDefault;
 import net.thenextlvl.protect.schematic.SchematicHolder;
-import org.bukkit.Location;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.UUID;
 
 /**
  * The RegionizedArea interface represents an area that is bound to a region.
@@ -39,6 +34,28 @@ public interface RegionizedArea<T extends Region> extends Area, SchematicHolder 
      * @return true if the region was successfully set, false otherwise
      */
     boolean setRegion(T region);
+
+    /**
+     * Retrieves the UUID of the owner of the Area.
+     *
+     * @return the UUID of the owner
+     */
+    @Nullable
+    UUID getOwner();
+
+    /**
+     * Checks if the area has an owner.
+     *
+     * @return true if the area has an owner, false otherwise
+     */
+    boolean hasOwner();
+
+    /**
+     * Sets the owner of the Area object.
+     *
+     * @param owner the UUID of the owner to set
+     */
+    void setOwner(@Nullable UUID owner);
 
     /**
      * Checks if the area is too big.
