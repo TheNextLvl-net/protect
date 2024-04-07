ef9f5a43554d89f51231edf83bf80f84ef40e28b
david <mrminecraft00@gmail.com>
2024-04-07 21:54:39 +0200

    fixed nullability

--- plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlagRegistry.java
+++ plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlagRegistry.java
@@ -1,9 +1,6 @@
 package net.thenextlvl.protect.flag;
 
 import com.google.common.base.Preconditions;
-import core.annotation.MethodsReturnNotNullByDefault;
-import core.annotation.ParametersAreNotNullByDefault;
-import core.annotation.TypesAreNotNullByDefault;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
@@ -11,9 +8,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@TypesAreNotNullByDefault
-@ParametersAreNotNullByDefault
-@MethodsReturnNotNullByDefault
 public class CraftFlagRegistry implements FlagRegistry {
     private final Map<NamespacedKey, Flag<?>> registry = new HashMap<>();
 
@@ -31,7 +25,7 @@ public class CraftFlagRegistry implements FlagRegistry {
 
     @Override
     @SuppressWarnings("unchecked")
-    public <T> Optional<Flag<T>> getFlag(NamespacedKey key) {
+    public <T> @NotNull Optional<Flag<T>> getFlag(@NotNull NamespacedKey key) {
         return Optional.ofNullable((Flag<T>) registry.get(key));
     }
 
@@ -45,7 +39,7 @@ public class CraftFlagRegistry implements FlagRegistry {
     }
 
     @Override
-    public void unregister(NamespacedKey flag) throws IllegalStateException {
+    public void unregister(@NotNull NamespacedKey flag) throws IllegalStateException {
         Preconditions.checkState(registry.containsKey(flag), "never registered flag: " + flag);
         registry.remove(flag);
     }
