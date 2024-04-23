f5793c06a7479b64b0fda3157d0a40fbee6b1221
david <mrminecraft00@gmail.com>
2024-04-23 22:12:29 +0200

    override equals and hash code methods

--- plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlag.java
+++ plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlag.java
@@ -3,10 +3,24 @@ package net.thenextlvl.protect.flag;
 import org.bukkit.NamespacedKey;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public record CraftFlag<T>(@NotNull NamespacedKey key, @NotNull Class<? extends T> type, T defaultValue) implements Flag<T> {
 
     @Override
     public int compareTo(@NotNull Flag<?> flag) {
         return key().compareTo(flag.key());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CraftFlag<?> craftFlag)) return false;
+        return Objects.equals(key, craftFlag.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(key);
     }
 }
