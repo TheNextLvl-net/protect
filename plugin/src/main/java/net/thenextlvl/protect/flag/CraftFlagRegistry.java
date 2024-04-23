6a05dfe67fde0f10ecb2c6098c383b9efdf64629
david <mrminecraft00@gmail.com>
2024-04-23 22:13:04 +0200

    internal flag registry improvements

--- plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlagRegistry.java
+++ plugin/src/main/java/net/thenextlvl/protect/flag/CraftFlagRegistry.java
@@ -1,6 +1,6 @@
 package net.thenextlvl.protect.flag;
 
-import com.google.common.base.Preconditions;
+import lombok.Getter;
 import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
@@ -8,44 +8,48 @@ import org.jetbrains.annotations.NotNull;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Getter
 public class CraftFlagRegistry implements FlagRegistry {
-    private final Map<NamespacedKey, Flag<?>> registry = new HashMap<>();
+    private final Map<Plugin, Set<Flag<?>>> registry = new HashMap<>();
 
     @Override
     public @NotNull Set<Flag<?>> getFlags() {
-        return new HashSet<>(registry.values());
+        return registry.values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
     }
 
     @Override
     public @NotNull Set<Flag<?>> getFlags(@NotNull Plugin plugin) {
-        return registry.values().stream()
-                .filter(flag -> flag.key().getKey().equals(plugin.getName().toLowerCase(Locale.ROOT)))
-                .collect(Collectors.toSet());
+        return registry.get(plugin);
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public <T> @NotNull Optional<Flag<T>> getFlag(@NotNull NamespacedKey key) {
-        return Optional.ofNullable((Flag<T>) registry.get(key));
+        return getFlags().stream()
+                .filter(flag -> flag.key().equals(key))
+                .map(flag -> (Flag<T>) flag)
+                .findAny();
     }
 
     @Override
     public @NotNull <T> Flag<T> register(@NotNull Plugin plugin, @NotNull Class<? extends T> type, @NotNull String name, T defaultValue) throws IllegalStateException {
         var key = new NamespacedKey(plugin, name);
-        Preconditions.checkState(!registry.containsKey(key), "already registered flag: " + key);
         var flag = new CraftFlag<>(key, type, defaultValue);
-        registry.put(key, flag);
-        return flag;
+        if (registry.computeIfAbsent(plugin, p -> new HashSet<>()).add(flag)) return flag;
+        throw new IllegalStateException("already registered flag: " + key);
     }
 
     @Override
-    public void unregister(@NotNull NamespacedKey flag) throws IllegalStateException {
-        Preconditions.checkState(registry.containsKey(flag), "never registered flag: " + flag);
-        registry.remove(flag);
+    public boolean unregister(@NotNull NamespacedKey key) {
+        for (var flags : registry.values()) if (flags.removeIf(flag -> flag.key().equals(key))) return true;
+        return false;
     }
 
     @Override
-    public void unregisterAll(@NotNull Plugin plugin) {
-        registry.keySet().removeIf(key -> key.getKey().equals(plugin.getName().toLowerCase(Locale.ROOT)));
+    public boolean unregisterAll(@NotNull Plugin plugin) {
+        var remove = registry.remove(plugin);
+        return remove != null && !remove.isEmpty();
     }
 }
