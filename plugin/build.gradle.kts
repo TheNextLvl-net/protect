23e14c2195511545032388a0a9da25e6accb7126
david <mrminecraft00@gmail.com>
2024-05-01 12:09:24 +0200

    updated cloud

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
 
     implementation("org.bstats:bstats-bukkit:3.0.2")
-    implementation("org.incendo:cloud-paper:2.0.0-beta.5")
+    implementation("org.incendo:cloud-paper:2.0.0-SNAPSHOT")
     implementation("org.incendo:cloud-minecraft-extras:2.0.0-beta.5")
     implementation(platform("com.intellectualsites.bom:bom-newest:1.43"))
 
