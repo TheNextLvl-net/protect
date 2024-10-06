3cc63450e0b07eb6886383cd5355bfbc28078d42
david <mrminecraft00@gmail.com>
2024-10-06 12:34:05 +0200

    Switch to Paper-Brigadier for command registration
    Replaced Cloud command manager with Paper-Brigadier for handling commands. Simplified the AreaCommand class and improved code maintainability by removing redundant code. Updated the registration process to leverage Paper's lifecycle events.

--- plugin/src/main/java/net/thenextlvl/protect/command/AreaCommand.java
+++ plugin/src/main/java/net/thenextlvl/protect/command/AreaCommand.java
@@ -1,58 +1,34 @@
 package net.thenextlvl.protect.command;
 
-import io.papermc.paper.command.brigadier.CommandSourceStack;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import lombok.RequiredArgsConstructor;
 import net.thenextlvl.protect.ProtectPlugin;
-import org.incendo.cloud.description.Description;
-import org.incendo.cloud.exception.InvalidCommandSenderException;
-import org.incendo.cloud.exception.InvalidSyntaxException;
-import org.incendo.cloud.exception.NoPermissionException;
-import org.incendo.cloud.execution.ExecutionCoordinator;
-import org.incendo.cloud.minecraft.extras.MinecraftExceptionHandler;
-import org.incendo.cloud.paper.PaperCommandManager;
 
 @SuppressWarnings("UnstableApiUsage")
+@RequiredArgsConstructor
 public class AreaCommand {
-    private final PaperCommandManager<CommandSourceStack> commandManager;
     private final ProtectPlugin plugin;
 
-    public AreaCommand(ProtectPlugin plugin) {
-        this.commandManager = PaperCommandManager.builder()
-                .executionCoordinator(ExecutionCoordinator.simpleCoordinator())
-                .buildOnEnable(plugin);
-        MinecraftExceptionHandler.create(CommandSourceStack::getSender)
-                .handler(InvalidSyntaxException.class, (formatter, context) -> plugin.bundle().miniMessage()
-                        .deserialize("<prefix> <red>/" + context.exception().correctSyntax()
-                                .replace("(", "<dark_gray>(<gold>").replace(")", "<dark_gray>)")
-                                .replace("[", "<dark_gray>[<gold>").replace("]", "<dark_gray>]")
-                                .replace("|", "<dark_gray>|<red>")))
-                .handler(InvalidCommandSenderException.class, (formatter, exception) -> plugin.bundle()
-                        .component(exception.context().sender().getSender(), "command.sender"))
-                .handler(NoPermissionException.class, (formatter, exception) -> plugin.bundle().component(
-                        exception.context().sender().getSender(), "command.permission", Placeholder.parsed("permission",
-                                exception.exception().missingPermission().permissionString())))
-                .registerTo(commandManager);
-        commandManager.commandSyntaxFormatter(new CustomSyntaxFormatter<>(commandManager));
-        this.plugin = plugin;
-    }
-
     public void register() {
-        var area = commandManager.commandBuilder("area")
-                .commandDescription(Description.description("create, delete or manage areas"));
-        commandManager.command(new AreaCreateCommand(plugin, area).create());
-        commandManager.command(new AreaDeleteCommand(plugin, area).create());
-        commandManager.command(new AreaFlagCommand.Info(plugin, area).create());
-        commandManager.command(new AreaFlagCommand.List(plugin, area).create());
-        commandManager.command(new AreaFlagCommand.Set(plugin, area).create());
-        commandManager.command(new AreaFlagCommand.Unset(plugin, area).create());
-        commandManager.command(new AreaInfoCommand(plugin, area).create());
-        commandManager.command(new AreaListCommand(plugin, area).create());
-        commandManager.command(new AreaPriorityCommand(plugin, area).create());
-        commandManager.command(new AreaRedefineCommand(plugin, area).create());
-        commandManager.command(new AreaSchematicCommand.Delete(plugin, area).create());
-        commandManager.command(new AreaSchematicCommand.Load(plugin, area).create());
-        commandManager.command(new AreaSchematicCommand.Save(plugin, area).create());
-        commandManager.command(new AreaSelectCommand(plugin, area).create());
-        commandManager.command(new AreaTeleportCommand(plugin, area).create());
+        plugin.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS.newHandler(event ->
+                event.registrar().register(Commands.literal("area")
+                        .requires(stack -> stack.getSender().hasPermission("protect.command.area"))
+                        .then(new AreaCreateCommand(plugin).create())
+                        .then(new AreaDeleteCommand(plugin).create())
+                        .then(new AreaFlagCommand.Info(plugin).create())
+                        .then(new AreaFlagCommand.List(plugin).create())
+                        .then(new AreaFlagCommand.Set(plugin).create())
+                        .then(new AreaFlagCommand.Unset(plugin).create())
+                        .then(new AreaInfoCommand(plugin).create())
+                        .then(new AreaListCommand(plugin).create())
+                        .then(new AreaPriorityCommand(plugin).create())
+                        .then(new AreaRedefineCommand(plugin).create())
+                        .then(new AreaSchematicCommand.Delete(plugin).create())
+                        .then(new AreaSchematicCommand.Load(plugin).create())
+                        .then(new AreaSchematicCommand.Save(plugin).create())
+                        .then(new AreaSelectCommand(plugin).create())
+                        .then(new AreaTeleportCommand(plugin).create())
+                        .build())));
     }
 }
