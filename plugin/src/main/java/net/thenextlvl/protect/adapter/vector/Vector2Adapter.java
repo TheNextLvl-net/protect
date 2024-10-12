c1fb4e55e54c7702000e881cd82f9d67ac6052c0
david <mrminecraft00@gmail.com>
2024-10-12 19:13:22 +0200

    Add Vector2Adapter for JSON serialization/deserialization
    This commit introduces a new class `Vector2Adapter` which implements `JsonSerializer<Vector2>` and `JsonDeserializer<Vector2>`. This adapter facilitates converting `Vector2` objects to and from JSON format, ensuring smooth integration with existing JSON data handling processes.

---
+++ plugin/src/main/java/net/thenextlvl/protect/adapter/vector/Vector2Adapter.java
@@ -0,0 +1,22 @@
+package net.thenextlvl.protect.adapter.vector;
+
+import com.google.gson.*;
+import com.sk89q.worldedit.math.Vector2;
+
+import java.lang.reflect.Type;
+
+public class Vector2Adapter implements JsonSerializer<Vector2>, JsonDeserializer<Vector2> {
+
+    @Override
+    public Vector2 deserialize(JsonElement element, Type type, JsonDeserializationContext context) throws JsonParseException {
+        var split = element.getAsString().split(", ");
+        var x = Double.parseDouble(split[0]);
+        var z = Double.parseDouble(split[1]);
+        return Vector2.at(x, z);
+    }
+
+    @Override
+    public JsonElement serialize(Vector2 vector, Type type, JsonSerializationContext context) {
+        return new JsonPrimitive(vector.x() + ", " + vector.z());
+    }
+}
