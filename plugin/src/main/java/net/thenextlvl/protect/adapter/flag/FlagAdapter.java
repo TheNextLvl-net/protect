78df5b003517b662f0d25635b2f705d51220a622
david <mrminecraft00@gmail.com>
2024-10-13 11:44:34 +0200

    Add null-safety annotations to flag adapter classes
    Moved null-safety annotations from package-info.java to individual classes: FlagsAdapter and FlagAdapter. Additionally, introduced @NotNull annotation in the serialize method of FlagAdapter.

--- plugin/src/main/java/net/thenextlvl/protect/adapter/flag/FlagAdapter.java
+++ plugin/src/main/java/net/thenextlvl/protect/adapter/flag/FlagAdapter.java
@@ -1,19 +1,24 @@
 package net.thenextlvl.protect.adapter.flag;
 
 import com.google.gson.*;
+import core.annotation.FieldsAreNotNullByDefault;
+import core.annotation.ParametersAreNotNullByDefault;
 import lombok.RequiredArgsConstructor;
 import net.thenextlvl.protect.ProtectPlugin;
 import net.thenextlvl.protect.flag.Flag;
 import org.bukkit.NamespacedKey;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
 
 @RequiredArgsConstructor
+@FieldsAreNotNullByDefault
+@ParametersAreNotNullByDefault
 public class FlagAdapter implements JsonSerializer<Flag<?>>, JsonDeserializer<Flag<?>> {
     private final ProtectPlugin plugin;
 
     @Override
-    public JsonElement serialize(Flag<?> flag, Type type, JsonSerializationContext context) {
+    public @NotNull JsonElement serialize(Flag<?> flag, Type type, JsonSerializationContext context) {
         return new JsonPrimitive(flag.key().asString());
     }
 
