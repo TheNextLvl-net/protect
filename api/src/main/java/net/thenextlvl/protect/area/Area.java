33538ce77008768e212f38274151fec9dc5f31f9
david <mrminecraft00@gmail.com>
2024-10-20 12:56:48 +0200

    Add parent area management to Area interface
    Introduce methods for setting and retrieving parent areas, enhancing hierarchical area management. This change allows areas to inherit values, such as flags, from their parent areas, improving flexibility and organization.

--- api/src/main/java/net/thenextlvl/protect/area/Area.java
+++ api/src/main/java/net/thenextlvl/protect/area/Area.java
@@ -3,14 +3,24 @@ package net.thenextlvl.protect.area;
 import core.annotation.MethodsReturnNotNullByDefault;
 import net.thenextlvl.protect.flag.FlagProvider;
 import org.bukkit.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.Optional;
 
 /**
  * The Area interface represents an area inside a world.
  */
 @MethodsReturnNotNullByDefault
 public interface Area extends Container, FlagProvider, Comparable<Area> {
+    /**
+     * Retrieves the parent area of this area.
+     * The parent area inherits its values, such as flags, to this area.
+     *
+     * @return an Optional containing the parent area if it exists, otherwise an empty Optional
+     */
+    Optional<String> getParent();
+
     /**
      * Retrieves the name of the area.
      *
@@ -21,21 +31,36 @@ public interface Area extends Container, FlagProvider, Comparable<Area> {
 
     /**
      * Retrieves the priority of the area.
+     * <p>
      * The priority determines the order in which areas are considered when resolving conflicts or determining containment.
      * A higher priority value indicates a higher priority for the area.
-     * <p>
-     * Note: This method does not validate the uniqueness of the priority value within the same container.
      *
      * @return the priority of the area
      */
     int getPriority();
 
     /**
+     * Sets the parent area of this area.
+     * The parent area inherits its values, such as flags, to this area.
+     *
+     * @param parent the parent area to set, or null to clear the parent area
+     * @return true if the parent area was successfully set, false otherwise
+     */
+    boolean setParent(@Nullable Area parent);
+
+    /**
+     * Sets the parent area of this area by its name pattern.
+     * The parent area inherits its values, such as flags, to this area.
+     *
+     * @param parent the name pattern of the parent area to set, or null to clear the parent area
+     * @return true if the parent area was successfully set, false otherwise
+     */
+    boolean setParent(@Nullable @NamePattern String parent);
+
+    /**
      * Sets the priority of the area.
      * The priority determines the order in which areas are considered when resolving conflicts or determining containment.
      * A higher priority value indicates a higher priority for the area.
-     * <p>
-     * Note: This method does not validate the uniqueness of the priority value within the same container.
      *
      * @param priority the priority value to set
      * @return whether the priority was changed
