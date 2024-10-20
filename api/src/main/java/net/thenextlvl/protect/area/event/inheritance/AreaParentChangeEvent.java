e78e48125ff0a6fc4f3dbb673f86c9fb29367d2e
david <mrminecraft00@gmail.com>
2024-10-20 12:56:56 +0200

    Add AreaParentChangeEvent for tracking parent changes
    Introduce the `AreaParentChangeEvent` class to handle events when an area's parent is changed. This event is cancellable and contains relevant data such as the new parent and the affected area.

---
+++ api/src/main/java/net/thenextlvl/protect/area/event/inheritance/AreaParentChangeEvent.java
@@ -0,0 +1,24 @@
+package net.thenextlvl.protect.area.event.inheritance;
+
+import lombok.Getter;
+import lombok.Setter;
+import net.thenextlvl.protect.area.Area;
+import net.thenextlvl.protect.area.event.AreaEvent;
+import org.bukkit.event.Cancellable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * This event is fired when the parent of an area is changed.
+ */
+@Getter
+@Setter
+public class AreaParentChangeEvent extends AreaEvent<@NotNull Area> implements Cancellable {
+    private boolean cancelled;
+    private @Nullable String parent;
+
+    public AreaParentChangeEvent(@NotNull Area area, @Nullable String parent) {
+        super(area);
+        this.parent = parent;
+    }
+}
