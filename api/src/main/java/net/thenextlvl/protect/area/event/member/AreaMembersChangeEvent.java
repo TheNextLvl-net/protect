d27f07fb533bc57a8589ba1eaeba35268d99464b
david <mrminecraft00@gmail.com>
2024-10-20 12:57:21 +0200

    Add AreaMembersChangeEvent for tracking member changes
    Introduce a new event class to handle changes in area members. This event is cancellable and ensures member modifications can be effectively managed.

---
+++ api/src/main/java/net/thenextlvl/protect/area/event/member/AreaMembersChangeEvent.java
@@ -0,0 +1,32 @@
+package net.thenextlvl.protect.area.event.member;
+
+import core.annotation.ParametersAreNotNullByDefault;
+import core.annotation.TypesAreNotNullByDefault;
+import lombok.Getter;
+import lombok.Setter;
+import net.thenextlvl.protect.area.RegionizedArea;
+import net.thenextlvl.protect.area.event.AreaEvent;
+import org.bukkit.event.Cancellable;
+
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * An event that represents a change in the members of an area.
+ * Cancelling this event results in the members not being set.
+ *
+ * @param <T> The type of the area associated with this event
+ */
+@Getter
+@Setter
+@TypesAreNotNullByDefault
+@ParametersAreNotNullByDefault
+public class AreaMembersChangeEvent extends AreaEvent<RegionizedArea<?>> implements Cancellable {
+    private Set<UUID> members;
+    private boolean cancelled;
+
+    public AreaMembersChangeEvent(RegionizedArea<?> area, Set<UUID> members) {
+        super(area);
+        this.members = members;
+    }
+}
