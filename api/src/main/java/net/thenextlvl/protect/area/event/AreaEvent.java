2609ffc17f73aa776d922aff1e275a425a3c95b2
david <mrminecraft00@gmail.com>
2024-10-24 18:12:15 +0200

    Remove custom null-safety annotations
    Replaced custom null-safety annotations with `@NotNull` from JetBrains. This promotes consistency and makes use of a widely-adopted standard. Updated relevant fields and methods to reflect this change.

--- api/src/main/java/net/thenextlvl/protect/area/event/AreaEvent.java
+++ api/src/main/java/net/thenextlvl/protect/area/event/AreaEvent.java
@@ -1,15 +1,12 @@
 package net.thenextlvl.protect.area.event;
 
-import core.annotation.FieldsAreNotNullByDefault;
-import core.annotation.MethodsReturnNotNullByDefault;
-import core.annotation.ParametersAreNotNullByDefault;
-import core.annotation.TypesAreNotNullByDefault;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.thenextlvl.protect.area.Area;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This class represents an abstract base class for events related to areas.
@@ -17,17 +14,13 @@ import org.bukkit.event.HandlerList;
  * @param <T> The type of the area of this event
  */
 @Getter
-@TypesAreNotNullByDefault
-@FieldsAreNotNullByDefault
-@MethodsReturnNotNullByDefault
-@ParametersAreNotNullByDefault
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AreaEvent<T extends Area> extends Event {
     private static final @Getter HandlerList handlerList = new HandlerList();
-    private final T area;
+    private final @NotNull T area;
 
     @Override
-    public HandlerList getHandlers() {
+    public @NotNull HandlerList getHandlers() {
         return getHandlerList();
     }
 }
