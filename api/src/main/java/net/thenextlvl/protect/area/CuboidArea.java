6dfbf48a6e133b0df73a9e79412253cf6ddf6b46
david <mrminecraft00@gmail.com>
2024-10-31 19:49:24 +0100

    Add @NullMarked annotations to area interfaces
    Included @NullMarked annotations to EllipsoidArea, CylinderArea, CuboidArea, and GroupedArea interfaces. This ensures better nullability checks and code safety in regions-related classes.

--- api/src/main/java/net/thenextlvl/protect/area/CuboidArea.java
+++ api/src/main/java/net/thenextlvl/protect/area/CuboidArea.java
@@ -1,9 +1,11 @@
 package net.thenextlvl.protect.area;
 
 import com.sk89q.worldedit.regions.CuboidRegion;
+import org.jspecify.annotations.NullMarked;
 
 /**
  * CuboidArea is an interface that represents an area bound to a CuboidRegion.
  */
+@NullMarked
 public interface CuboidArea extends RegionizedArea<CuboidRegion> {
 }
--- api/src/main/java/net/thenextlvl/protect/area/EllipsoidArea.java
+++ api/src/main/java/net/thenextlvl/protect/area/EllipsoidArea.java
@@ -1,9 +1,11 @@
 package net.thenextlvl.protect.area;
 
 import com.sk89q.worldedit.regions.EllipsoidRegion;
+import org.jspecify.annotations.NullMarked;
 
 /**
  * EllipsoidArea is an interface that represents an area bound to an EllipsoidRegion.
  */
+@NullMarked
 public interface EllipsoidArea extends RegionizedArea<EllipsoidRegion> {
 }
