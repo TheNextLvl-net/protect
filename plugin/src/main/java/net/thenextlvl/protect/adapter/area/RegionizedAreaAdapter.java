401d990c4ca32e78eadd12148acbc037b1d99bec
david <mrminecraft00@gmail.com>
2024-11-01 22:14:08 +0100

    Remove JSON serialization/deserialization
    Eliminates the JSON serialization and deserialization logic from the `RegionizedAreaAdapter` class. This simplifies the class and removes dependencies on unnecessary imports, improving readability and maintainability.

--- plugin/src/main/java/net/thenextlvl/protect/adapter/area/RegionizedAreaAdapter.java
+++ plugin/src/main/java/net/thenextlvl/protect/adapter/area/RegionizedAreaAdapter.java
@@ -1,25 +1,15 @@
 package net.thenextlvl.protect.adapter.area;
 
-import com.google.common.reflect.TypeToken;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
 import com.sk89q.worldedit.regions.Region;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import net.kyori.adventure.key.KeyPattern;
 import net.thenextlvl.protect.ProtectPlugin;
 import net.thenextlvl.protect.area.AreaCreator;
-import net.thenextlvl.protect.area.CraftAreaCreator;
 import net.thenextlvl.protect.area.CraftRegionizedArea;
-import net.thenextlvl.protect.flag.Flag;
 import net.thenextlvl.protect.io.AreaAdapter;
 import org.bukkit.NamespacedKey;
-import org.bukkit.World;
 import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
-
-import java.util.*;
 
 @NullMarked
 @Accessors(fluent = true)
@@ -34,33 +24,4 @@ public abstract class RegionizedAreaAdapter<C extends Region, T extends CraftReg
     }
 
     protected abstract T construct(AreaCreator<C> creator);
-
-    public T deserialize(JsonObject object, World world, JsonDeserializationContext context) {
-        var name = object.get("name").getAsString();
-        var priority = object.get("priority").getAsInt();
-
-        var parent = object.has("parent") ? object.get("parent").getAsString() : null;
-        var owner = object.has("owner") ? context.<UUID>deserialize(object.get("owner"), UUID.class) : null;
-
-        var members = Objects.<Set<UUID>>requireNonNullElseGet(context.deserialize(object.get("members"), new TypeToken<Set<UUID>>() {
-        }.getType()), HashSet::new);
-        var flags = context.<Map<Flag<?>, @Nullable Object>>deserialize(object.get("flags"), LinkedHashMap.class);
-
-        var region = context.<C>deserialize(object.get("region"), new TypeToken<C>(getClass()) {
-        }.getType());
-
-        return construct(new CraftAreaCreator<>(plugin, name, world, region, parent, owner, flags, members, priority));
-    }
-
-    public JsonObject serialize(T area, JsonSerializationContext context) {
-        var object = new JsonObject();
-        object.addProperty("name", area.getName());
-        object.addProperty("priority", area.getPriority());
-        area.getOwner().ifPresent(owner -> object.add("owner", context.serialize(owner)));
-        area.parent().ifPresent(parent -> object.addProperty("parent", parent));
-        object.add("members", context.serialize(area.getMembers()));
-        object.add("region", context.serialize(area.getRegion()));
-        object.add("flags", context.serialize(area.getFlags()));
-        return object;
-    }
 }
