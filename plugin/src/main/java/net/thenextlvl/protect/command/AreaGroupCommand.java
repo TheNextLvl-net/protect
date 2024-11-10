26c28af1abbe840c02bc48fc92255cd043566fb9
david <mrminecraft00@gmail.com>
2024-11-10 16:31:17 +0100

    Refactor AreaGroupCommand structure and add redefine action
    Refactor the AreaGroupCommand class by extracting command definitions into separate methods, improving readability and maintainability. Also, introduce a new 'redefine' command to enhance area group management functionalities.

--- plugin/src/main/java/net/thenextlvl/protect/command/AreaGroupCommand.java
+++ plugin/src/main/java/net/thenextlvl/protect/command/AreaGroupCommand.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.sk89q.worldedit.IncompleteRegionException;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
@@ -28,48 +29,65 @@ public class AreaGroupCommand {
     LiteralArgumentBuilder<CommandSourceStack> create() {
         return Commands.literal("group")
                 .requires(stack -> stack.getSender().hasPermission("protect.command.area.group"))
-                .then(Commands.literal("delete")
-                        .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.delete"))
-                        .then(Commands.argument("group", new GroupedAreaArgumentType(plugin))
-                                .executes(this::delete)))
-                .then(Commands.literal("create")
-                        .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.create"))
-                        .then(Commands.argument("area", new RegionizedAreaArgumentType(plugin,
-                                        regionizedArea -> !(regionizedArea instanceof GroupedArea)))
-                                .executes(this::create)))
-                .then(Commands.literal("add")
-                        .requires(stack -> stack.getSender() instanceof Player player
-                                           && player.hasPermission("protect.command.area.group.add"))
-                        .then(Commands.argument("area", new GroupedAreaArgumentType(plugin))
-                                .then(Commands.argument("name", StringArgumentType.string())
-                                        .executes(context -> {
-                                            var name = context.getArgument("name", String.class);
-                                            return add(context, name);
-                                        }))
-                                .executes(context -> {
-                                    var area = context.getArgument("area", GroupedArea.class);
-                                    var name = "region-" + (area.getRegion().getRegions().size() + 1);
-                                    while (area.getRegion().hasRegion(name)) name = name + ".1";
-                                    return add(context, name);
-                                })))
-                .then(Commands.literal("inspect")
-                        .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.inspect"))
-                        .then(Commands.argument("area", new GroupedAreaArgumentType(plugin))
-                                .executes(this::inspect)))
-                .then(Commands.literal("remove")
-                        .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.remove"))
-                        .then(Commands.argument("area", new GroupedAreaArgumentType(plugin))
-                                .then(Commands.argument("region", StringArgumentType.string())
-                                        .suggests((context, builder) -> {
-                                            var group = context.getChild().getArgument("area", GroupedArea.class);
-                                            group.getRegion().getRegions().keySet().forEach(builder::suggest);
-                                            return builder.buildFuture();
-                                        }).executes(this::remove))));
+                .then(delete())
+                .then(groupCreate())
+                .then(inspect())
+                .then(redefine())
+                .then(remove());
+    }
+
+    private LiteralArgumentBuilder<CommandSourceStack> delete() {
+        return Commands.literal("delete")
+                .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.delete"))
+                .then(Commands.argument("group", new GroupedAreaArgumentType(plugin))
+                        .executes(this::delete));
+    }
+
+    private LiteralArgumentBuilder<CommandSourceStack> groupCreate() {
+        return Commands.literal("create")
+                .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.create"))
+                .then(Commands.argument("area", new RegionizedAreaArgumentType(plugin,
+                                regionizedArea -> !(regionizedArea instanceof GroupedArea)))
+                        .executes(this::create));
+    }
+
+    private LiteralArgumentBuilder<CommandSourceStack> inspect() {
+        return Commands.literal("inspect")
+                .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.inspect"))
+                .then(Commands.argument("area", new GroupedAreaArgumentType(plugin))
+                        .executes(this::inspect));
+    }
+
+    private LiteralArgumentBuilder<CommandSourceStack> redefine() {
+        return Commands.literal("redefine")
+                .requires(stack -> stack.getSender() instanceof Player player
+                                   && player.hasPermission("protect.command.area.group.redefine"))
+                .then(Commands.argument("group", new GroupedAreaArgumentType(plugin))
+                        .then(Commands.argument("region", StringArgumentType.string())
+                                .suggests(suggestGroups())
+                                .executes(this::redefine)));
+    }
+
+    private LiteralArgumentBuilder<CommandSourceStack> remove() {
+        return Commands.literal("remove")
+                .requires(stack -> stack.getSender().hasPermission("protect.command.area.group.remove"))
+                .then(Commands.argument("group", new GroupedAreaArgumentType(plugin))
+                        .then(Commands.argument("region", StringArgumentType.string())
+                                .suggests(suggestGroups())
+                                .executes(this::remove)));
+    }
+
+    private static SuggestionProvider<CommandSourceStack> suggestGroups() {
+        return (context, builder) -> {
+            var group = context.getChild().getArgument("group", GroupedArea.class);
+            group.getRegion().getRegions().keySet().forEach(builder::suggest);
+            return builder.buildFuture();
+        };
     }
 
     private int remove(CommandContext<CommandSourceStack> context) {
         var sender = context.getSource().getSender();
-        var area = context.getArgument("area", GroupedArea.class);
+        var area = context.getArgument("group", GroupedArea.class);
         var region = context.getArgument("region", String.class);
         var success = area.getRegion().removeRegion(region);
         var message = success ? "area.group.remove" : "nothing.changed";
@@ -77,6 +95,10 @@ public class AreaGroupCommand {
                 Placeholder.parsed("area", area.getName()),
                 Placeholder.parsed("region", region));
         return success ? Command.SINGLE_SUCCESS : 0;
+    }
+
+    private int redefine(CommandContext<CommandSourceStack> context) {
+        return Command.SINGLE_SUCCESS;
     }
 
     private int inspect(CommandContext<CommandSourceStack> context) {
@@ -89,20 +111,20 @@ public class AreaGroupCommand {
             var session = WorldEditPlugin.getInstance().getSession(player);
             var region = session.getSelection();
 
-            var area = context.getArgument("area", GroupedArea.class);
+            var group = context.getArgument("group", GroupedArea.class);
 
-            if (!area.getRegion().getRegions().values().iterator().next().getClass().isInstance(region)) {
+            if (!group.getRegion().getRegions().values().iterator().next().getClass().isInstance(region)) {
                 plugin.bundle().sendMessage(player, "region.unsupported",
                         Placeholder.parsed("type", region.getClass().getSimpleName()));
                 return 0;
             }
 
-            var success = !area.getRegion().hasRegion(region)
-                          && area.getRegion().addRegion(name, region);
+            var success = !group.getRegion().hasRegion(region)
+                          && group.getRegion().addRegion(name, region);
 
             var message = success ? "area.group.add" : "nothing.changed";
             plugin.bundle().sendMessage(player, message,
-                    Placeholder.parsed("area", area.getName()),
+                    Placeholder.parsed("area", group.getName()),
                     Placeholder.parsed("region", name));
             return success ? Command.SINGLE_SUCCESS : 0;
         } catch (IncompleteRegionException ignored) {
@@ -112,21 +134,39 @@ public class AreaGroupCommand {
     }
 
     private int delete(CommandContext<CommandSourceStack> context) {
+        var sender = context.getSource().getSender();
+        var group = context.getArgument("group", GroupedArea.class);
+
+        if (!plugin.areaService().delete(group)) {
+            plugin.bundle().sendMessage(sender, "area.group.delete.failed",
+                    Placeholder.parsed("group", group.getName()));
+            return 0;
+        }
+
+        var region = group.getRegion().getRegions().values().iterator().next();
+        var area = plugin.areaService().creator(group)
+                .region(region)
+                .create();
+
+        plugin.bundle().sendMessage(sender, "area.group.delete",
+                Placeholder.parsed("group", area.getName()));
         return Command.SINGLE_SUCCESS;
     }
 
     private int create(CommandContext<CommandSourceStack> context) {
         var sender = context.getSource().getSender();
         var area = context.getArgument("area", RegionizedArea.class);
-        plugin.areaService().delete((RegionizedArea<?>) area);
-        var groups = new GroupedRegion(Map.of("root", area.getRegion()));
-        var grouped = plugin.areaService().creator(area.getName(), area.getWorld(), groups)
-                .flags(area.getFlags())
-                .members(area.getMembers())
-                .owner(area.getOwner().orElse(null))
-                .parent(area.getParent().orElse(null))
-                .priority(area.getPriority())
+
+        if (!plugin.areaService().delete((RegionizedArea<?>) area)) {
+            plugin.bundle().sendMessage(sender, "area.group.create.failed",
+                    Placeholder.parsed("area", area.getName()));
+            return 0;
+        }
+
+        var grouped = plugin.areaService().creator((RegionizedArea<?>) area)
+                .region(new GroupedRegion(Map.of("root", area.getRegion())))
                 .create();
+
         plugin.bundle().sendMessage(sender, "area.group.create",
                 Placeholder.parsed("area", grouped.getName()));
         return Command.SINGLE_SUCCESS;
