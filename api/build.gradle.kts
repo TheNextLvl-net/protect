eb9b9a0e9407e0e7ac5dd118ffeae754883bc403
david <github@nonswag.dev>
2025-06-19 07:10:39 +0200

    1.21.6

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 dependencies {
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Core")
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit") { isTransitive = false }
-    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.6-R0.1-SNAPSHOT")
 
     implementation("net.thenextlvl.core:nbt:2.3.2")
     implementation("net.thenextlvl.core:files:3.0.0")
