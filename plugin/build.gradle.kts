5c4c13ae7b5ac892cc173622bb7964f8010cb491
david <mail@nonswag.dev>
2025-09-26 18:56:56 +0200

    Downgrade bstats to 3.1.0

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     compileOnly("com.fastasyncworldedit:FastAsyncWorldEdit-Bukkit") { isTransitive = false }
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
 
-    implementation("org.bstats:bstats-bukkit:3.1.1-SNAPSHOT")
+    implementation("org.bstats:bstats-bukkit:3.1.0")
 
     implementation(project(":api"))
     implementation("net.thenextlvl.core:i18n:3.2.2")
