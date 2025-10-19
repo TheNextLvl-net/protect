88b40ba792a5e310492efa665e7fb10e142412fa
david <mail@nonswag.dev>
2025-10-19 10:20:38 +0200

    Enhanced area interaction and priority checks

--- src/main/java/net/thenextlvl/protect/area/CraftRegionizedArea.java
+++ src/main/java/net/thenextlvl/protect/area/CraftRegionizedArea.java
@@ -175,7 +175,8 @@ public abstract class CraftRegionizedArea<T extends Region> extends CraftArea im
     @Override
     public boolean canInteract(Area area) {
         return equals(area) || area.getParent().map(this::equals).orElse(false)
-               || (area instanceof RegionizedArea<?> regionized && Objects.equals(regionized.getOwner(), getOwner()));
+                || (area instanceof RegionizedArea<?> regionized && Objects.equals(regionized.getOwner(), getOwner()))
+                || getPriority() > area.getPriority();
     }
 
     @Override
