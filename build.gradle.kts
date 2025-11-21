943646a45a4ae44639f054f9640f45318322f9d3
david <mail@nonswag.dev>
2025-11-21 20:26:40 +0100

    Updated import paths and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,8 +39,8 @@ dependencies {
     implementation("org.bstats:bstats-bukkit:3.1.0")
 
     implementation(project(":api"))
-    implementation("net.thenextlvl.core:i18n:3.2.2")
     implementation("net.thenextlvl.core:paper:2.3.1")
+    implementation("net.thenextlvl:i18n:1.0.0")
 }
 
 tasks.shadowJar {
